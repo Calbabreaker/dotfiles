df89e4717b9a961c50e2977f485a9c79e1951b4c
Calbabreaker <calbabreaker@gmail.com>
2021-06-19 10:27:13 +0930

    restructered

--- setup.sh
+++ setup.sh
@@ -5,15 +5,17 @@ FOLDER=$(pwd)
 echo "Creating symbolic links to dotfiles..."
 ln -sf $FOLDER/.zshrc ~/.zshrc
 ln -sf $FOLDER/.gitconfig ~/.gitconfig
-ln -sf $FOLDER/init.vim ~/.config/nvim/init.vim
-ln -sf $FOLDER/coc-settings.json ~/.config/nvim/coc-settings.json
 
-echo "Installing plugins for vim..."
+ln -sf $FOLDER/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf $FOLDER/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+echo "Installing plug for vim..."
 if [ ! -e "$HOME/.vim/autoload/plug.vim" ]; then
     curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+echo "Installing zplug for zsh..."
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 echo "Done! Enjoy the dotfiles!"
