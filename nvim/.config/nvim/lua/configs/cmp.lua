e37eb792f3d8da4b52d8ca50ac54cbc3e607a961
Calbabreaker <calbabreaker@gmail.com>
2022-12-04 23:37:09 +1030

    fix: make cmp select more consisten

--- nvim/.config/nvim/lua/configs/cmp.lua
+++ nvim/.config/nvim/lua/configs/cmp.lua
@@ -1,13 +1,3 @@
-local function has_words_before()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local function has_space_before()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 local completion_icons = {
 	Text = "",
 	Method = "",
@@ -37,42 +27,51 @@ local completion_icons = {
 }
 
 local cmp = require("cmp")
+
+local function visible_then_select(choose_next, fallback)
+	if cmp.visible() then
+		if choose_next then
+			cmp.select_next_item()
+		else
+			cmp.select_prev_item()
+		end
+	else
+		fallback() -- The fallback function sends a already mapped key.
+	end
+end
+
 cmp.setup({
 	mapping = {
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-Space>"] = cmp.mapping.complete(),
+		["<A-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
-			local luasnip = require("luasnip")
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() and not has_space_before() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-			end
-		end, {
-			"i",
-			"s",
-		}),
-
+			visible_then_select(true, fallback)
+		end),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
+			visible_then_select(false, fallback)
+		end),
+		["<C-j>"] = cmp.mapping(function(fallback)
+			visible_then_select(true, fallback)
+		end),
+		["<C-k>"] = cmp.mapping(function(fallback)
+			visible_then_select(false, fallback)
+		end),
+		["<C-n>"] = cmp.mapping(function(fallback)
 			local luasnip = require("luasnip")
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			end
-		end, {
-			"i",
-			"s",
-		}),
+		end),
+		["<C-p>"] = cmp.mapping(function(fallback)
+			local luasnip = require("luasnip")
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end),
 	},
 	window = {
 		documentation = {
