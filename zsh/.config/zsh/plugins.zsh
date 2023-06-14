688c1d4be220de893d01620983947a69e94ee766
Calbabreaker <calbabreaker@gmail.com>
2023-06-15 09:00:16 +0930

    fix: merge

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -93,5 +93,4 @@ bindkey '^o' 'fzf-open'
 
 # pyenv stuff
 export PYENV_ROOT="$HOME/.local/share/pyenv"
-command -v pyenv >/dev/null || [ -d "$PYENV_ROOT/bin" ] export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+command -v pyenv > /dev/null || [ -d "$PYENV_ROOT/bin" ] && export PATH="$PYENV_ROOT/bin:$PATH" && eval "$(pyenv init -)"
